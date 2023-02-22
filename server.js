17ca94b2de638ec9a96bba562a56393832bd4189
Muhammed Shafi <shafivp78@gmail.com>
2023-02-23 01:37:23 +0530

    New edit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 const port= process.env.PORT || 3000;
 
 app.get("/", (req,res)=>{
-    res.send('This is a sample page for defuse round 3  ');
+    res.send('This is a sample page for defuse round 3  \n Your mission is to setup a circuit that takes in the input 0123 and gives the output 8338');
 })
 app.listen(port,()=>{console.log(`Listening on ${port}`);
 
