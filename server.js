3a7ff8f6346b339d7a2f67aca206f3e335a7ac1c
Muhammed Shafi <shafivp78@gmail.com>
2023-02-23 01:14:46 +0530

    Port listening added

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var app = express();
-
-app.listen(process.env.PORT|| 3000);
+const port= process.env.PORT || 3000;
 
 app.get("/", (req,res)=>{
     res.send('This is a sample page for defuse round 3  ');
 })
+app.listen(port,()=>{console.log(`Listening on ${port}`);
+
+}
+
+);
