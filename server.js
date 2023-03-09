53832ecf78f1f3217c95c0d76f18e5756a621da9
Muhammed Shafi <shafivp78@gmail.com>
2023-03-09 14:04:49 +0530

    09_March_23

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require("express");
 var app = express();
 const port= process.env.PORT || 3000;
 
-app.get("/", (req,res)=>{
-    res.send('This is a sample page for defuse round 3  \n Your mission is to setup a circuit that takes in the input 0123 and gives the output 8338');
+app.post("/15061999", (req,res)=>{
+    res.send('This is a sample page for defuse round 3  \n Your mission is to setup a random sequence genrator with the output 2,3,4,5,6 \n Also send this sequence to your team mate using the controller given to you.');
 })
 app.listen(port,()=>{console.log(`Listening on ${port}`);
 
