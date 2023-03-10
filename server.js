e6d20b584cb37fa7ec0b0a04e25296ab7e77f6a8
Muhammed Shafi <shafivp78@gmail.com>
2023-03-11 00:54:46 +0530

    Final Final

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 const port= process.env.PORT || 3000;
 
 app.post("/15061999", (req,res)=>{
-    res.send('Soldiers, our current communication RF channel has the risk of getting intercepted.\n Inorder to avoid this it is advised to use IR for short range communication. So split the team into two and use the microcontrollers to communicate between the subteams using IR.\n  We have developed a custom library to send the coded data from source to destination.\n Use the function encoder from the library  custom_encode to retrieve the data of the bomb location \nand send this data to the second half of the team using an IR LED and diode\n');
+    res.send('Soldiers, our current communication RF channel has the risk of getting intercepted.\n Inorder to avoid this it is advised to use IR for short range communication. So split the team into two and use the microcontrollers to communicate between the subteams using IR.\n  We have developed a custom library to send the coded data from source to destination.\n Use the function encoder from the library  custom_encode to retrieve the data of the bomb location and send this data to the second half of the team using an IR LED and diode\n \n The function encode() takes no parameterand returns and integer revealing the location. ');
 })
 app.listen(port,()=>{console.log(`Listening on ${port}`);
 
